5e31629bf50e03bd831953c6a744d2b94bd2a2eb
ImJayc2 <117063113+ImJayc2@users.noreply.github.com>
2023-02-25 19:49:53 +1300

    Update KiwiCalandar.lua

--- KiwiCalandar.lua
+++ KiwiCalandar.lua
@@ -3,3 +3,11 @@
 -- Oceanic Server time is AEST = UTC+10 or AEDT = UTC+11
 -- NZDT is UTC+13
 -- NZST is UTC+12
+--CalendarTime
+local ct = C_DateAndTime.GetCurrentCalendarTime()
+print(format("The Calendar time is %02d:%02d, %s, %d %s %d", ct.hour, ct.minute, weekDay, ct.monthDay, month, ct.year))
+--Server Time
+local st = C_DateAndTime.GetServerTime()
+print(format("The Server time is %02d:%02d, %s, %d %s %d", st.hour, st.minute, weekDay, st.monthDay, month, st.year))
+local lt = C_DateAndTime.GetServerTimeLocal()
+print(format("The Local time is %02d:%02d, %s, %d %s %d", lt.hour, lt.minute, weekDay, lt.monthDay, month, lt.year))
